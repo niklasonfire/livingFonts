8eb10b73a6fcdadf7fab4e45ee48e1cffc5206e5
niklasonfire <niklas200291@yahoo.de>
2018-12-13 13:30:58 +0100

    aktuelle Version

--- livingFonts.js
+++ livingFonts.js
@@ -1,17 +1,63 @@
 var smilie;
-var sliderSize;
-var sliderEyes;
+var words;
+var wordsRaw;
+var textInput;
+var textField;
+var enthalten;
+var word;
+var list;
+var v;
 
 function setup() {
-  createDiv("Größe");
-  sliderSize = createSlider(0, 100, 40, 0.1);
-  createDiv("Smile");
-  sliderSmile = createSlider(0, 100, 40, 0.1);
-  smilie = createP("Y");
-  smilie.style("font-family", "myFont");
+  noCanvas();
+  textInput = createInput();
+  textField = createP();
+  fillwordsRaw();
+  textField.style("font-family", "myFont");
+  textField.style("font-size", 40);
+  textField.style("transition", "all 2s");
+  textInput.input(getInput);
+
+
 }
 
+
 function draw() {
-  smilie.style("font-size", sliderSize.value());
-  smilie.style("font-variation-settings", " 'axxx'" + sliderSmile.value());
+
+
+  if (containsWord()) {
+    v = 150;
+    textField.style("font-variation-settings", "'wght'" + v);
+  } else {
+    v = 200;
+    textField.style("font-variation-settings", "'wght'" + v);
+  }
+
+}
+
+
+function getInput() {
+  textField.html(textInput.value());
+
+
+}
+
+function containsWord() {
+  var i;
+  for (i = 0; i < words.length; i++) {
+    if (textInput.value().includes(words[i])) {
+      return true
+    }
+  }
+
+  return false;
+}
+
+
+
+function fillwordsRaw() {
+  $("textField").load("badWords.txt");
+  //console.log(schtring);
+  words = "huhu", "huashd";
+
 }
