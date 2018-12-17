01bd1c788b000e44cd721a4ab086e9e2d504791b
niklasonfire <niklas200291@yahoo.de>
2018-12-17 17:14:18 +0100

    wörter wieder in js, stat .txt

--- livingFonts.js
+++ livingFonts.js
@@ -1,6 +1,5 @@
 var smilie;
 var words;
-var wordsRaw;
 var textInput;
 var textField;
 var enthalten;
@@ -56,8 +55,10 @@ function containsWord() {
 
 
 function fillwordsRaw() {
-  $("textField").load("badWords.txt");
-  //console.log(schtring);
-  words = "huhu", "huashd";
+  var txtFile = "halt,arrogant,abgespannt, aggressiv, alarmiert, angeekelt, angespannt, ängstlich, angstschlotternd, angreifend,ärgerlich, argwöhnisch, aufgebracht, ausgelaugtbeängstigt,bedrängt, bedrückt, befangen, befremdet, beklommen, bekümmert, belastet, beleidigt, beschämt, besorgt,  bestürzt, betroffen, betrübt, beunruhigt, bitter, blockiert, bösartig, brummig,  daneben, depressiv, deprimiert, distanziert, dumpf, durcheinander, eifersüchtig, einsam, ekelerfüllt, empfindlich,empört,energielos, entmutigt, entrüstet, entsetzt,  enttäuscht, ermüdet, ernüchtert, erregt, erschlagen, erschöpft, erschrocken, erzürnt,  feindselig, feststeckend, frustriert, furchtsam, feindlich,  gehässig, gehemmt, geladen, gelangweilt, gemein, gequält, gereizt, gestört, gewalttätig, gleichgültig, griesgrämig, grantig,  hasserfüllt, herabgewürdigt, hilfe, hilflos, hitzköpfig, hoffnungslos, hundsmiserabel, irritiert, jämmerlich,  kalt, kribbelig, kraftlos, launisch, leer, lüstern,lustlos,  masochistisch, mäkelnd, missmutig, misstrauisch, mies, müde, mürrisch, mutlos,  neidisch, nervös, niedergeschlagen, niederträchtig, nachtragend, ohnmächtig,  panisch, peinlich, perplex, pessimistisch, ratlos, ruhelos, rasend, rachsüchtig,  sauer, sadistisch, scheu, schläfrig, schlapp, schmollend, schockiert, schuldig,  selbstkritisch, selbstverachtend, scheußlich, schmerzerfüllt, schwermütig, schwunglos,  skeptisch, sorgenvoll, strapaziert, streitlustig, teilnahmslos, träge,traurig, trübselig,  überdrüssig, überfordert, überlastet, unangenehm, unbehaglich, unbeteiligt, ungeduldig, ungehalten,  ungemütlich, unglücklich, unklar, unnahbar, unruhig, unschlüssig, unsicher, unwohl, unzufrieden,  verängstigt, verärgert, verbittert, verkrampft, verlegen, verletzbar, verletzt, verloren, verrückt,  verschlafen, verschlossen, verschreckt, verspannt, verstimmt, verstört, verunsichert, verwirrt, verzagt,  verzweifelt,vorwurfsvoll,weinerlich,widerwillig,wütend,wutentbrannt, zappelig, zerknirscht, zermürbt, zerrissen, zittrig, zögerlich, zornig, zurückgewiesen, zynisch";
 
+  words = txtFile.split(",");
+  for (var i = 0; i < words.length; i++) {
+    words[i] = words[i].trim();
+  }
 }
