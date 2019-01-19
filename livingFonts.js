224a52b3686cf7771e8ba16c058ca2795fdc07f1
niklasonfire <niklas200291@yahoo.de>
2019-01-19 14:27:46 +0100

    190119

--- livingFonts.js
+++ livingFonts.js
@@ -1,65 +1,108 @@
-var words;
 var textInput;
 var textField;
 var enthalten;
-var word;
+var badWords;
 var inpWords;
 var list;
 var weight;
-
+var m;
 
 function setup() {
-  noCanvas();
-  textInput = createInput();
-  textField = createP();
+  createCanvas(windowWidth,windowHeight);
+  background(255,230,230);
+  headline  = createElement("H1");
+  headlineDiv = createDiv();
+  headlineDiv.child(headline);
+  headlineDiv.position(windowWidth/4+20,10);
+  headline.parent(headlineDiv);
+  headlineDiv.style("width","50%");
+  headline.html("the antianti font fuer mehr positives denken und selbstbewusstein");
+  headline.style("color","#4f1a27");
 
+  resetBtn = createButton("Reset ");
+  resetBtn.position((windowWidth/2)-330,windowHeight/4);
+  resetBtn.mousePressed(resetAll);
+
+  rectMode(CENTER);
+  noStroke();
+  textBox = rect(windowWidth/2,windowHeight/2+50,windowWidth/2,windowHeight/2);
+
+  textInput = createInput();
+  textInput.position((windowWidth/2)-250,windowHeight/4);
+  textInput.attribute("size",50);
+
+  // let textDiv = document.createElement("div");
+  // let textField = document.createElement("p");
+//  textDiv.appendChild(textField);
+  textDiv = createDiv();
+  textField =createP();
+  textDiv.child(textField);
+  textField.parent(textDiv);
+  //textDiv.style("width","100px");
+  textDiv.position(windowWidth/4+5,windowHeight/4+30);
+  textDiv.style("width","50%");
   fillwordsRaw();
   textField.style("font-family", "myFont");
   textField.style("font-size", 40);
   textField.style("transition", "all 2s");
-  textInput.input(getInput);
-  weight = 150;
+//  textField.position(windowWidth/4+5,windowHeight/4);
 
+
+  textInput.input(getInput);
+  m=0;
+  list = [];
+  v=200;
 }
 
 
 function draw() {
 
   if (containsWord()) {
-    v = 150;
+    v = 200-list.length*10
     textField.style("font-variation-settings", "'wght'" + v);
   } else {
-    v = 200;
+
     textField.style("font-variation-settings", "'wght'" + v);
   }
 
-
 }
+
 
 function getInput() {
   textField.html(textInput.value());
+
 }
 
-function containsWord() {
 
+function containsWord() {
   inpWords = textInput.value().split(" ");
-  for (var i = 0; i < words.length; i++) {
-    if (words[i] === inpWords[inpWords.length-1]) {
+
+
+  for(var i = 0; i < inpWords.length; i++) {
+    for(var j = 0;j<badWords.length;j++){
+      if ( badWords[j]===inpWords[i] && inpWords[i]!=list[i]) {
+      list[m++] = inpWords[i];
       return true;
+      }
     }
   }
 
   return false;
 }
 
+function resetAll(){
+  location.reload();
+
+}
+
 
 
 function fillwordsRaw() {
-  var txtFile = "halt,arrogant,abgespannt, aggressiv, alarmiert, angeekelt, angespannt, ängstlich, angstschlotternd, angreifend,ärgerlich, argwöhnisch, aufgebracht, ausgelaugtbeängstigt,bedrängt, bedrückt, befangen, befremdet, beklommen, bekümmert, belastet, beleidigt, beschämt, besorgt,  bestürzt, betroffen, betrübt, beunruhigt, bitter, blockiert, bösartig, brummig,  daneben, depressiv, deprimiert, distanziert, dumpf, durcheinander, eifersüchtig, einsam, ekelerfüllt, empfindlich,empört,energielos, entmutigt, entrüstet, entsetzt,  enttäuscht, ermüdet, ernüchtert, erregt, erschlagen, erschöpft, erschrocken, erzürnt,  feindselig, feststeckend, frustriert, furchtsam, feindlich,  gehässig, gehemmt, geladen, gelangweilt, gemein, gequält, gereizt, gestört, gewalttätig, gleichgültig, griesgrämig, grantig,  hasserfüllt, herabgewürdigt, hilfe, hilflos, hitzköpfig, hoffnungslos, hundsmiserabel, irritiert, jämmerlich,  kalt, kribbelig, kraftlos, launisch, leer, lüstern,lustlos,  masochistisch, mäkelnd, missmutig, misstrauisch, mies, müde, mürrisch, mutlos,  neidisch, nervös, niedergeschlagen, niederträchtig, nachtragend, ohnmächtig,  panisch, peinlich, perplex, pessimistisch, ratlos, ruhelos, rasend, rachsüchtig,  sauer, sadistisch, scheu, schläfrig, schlapp, schmollend, schockiert, schuldig,  selbstkritisch, selbstverachtend, scheußlich, schmerzerfüllt, schwermütig, schwunglos,  skeptisch, sorgenvoll, strapaziert, streitlustig, teilnahmslos, träge,traurig, trübselig,  überdrüssig, überfordert, überlastet, unangenehm, unbehaglich, unbeteiligt, ungeduldig, ungehalten,  ungemütlich, unglücklich, unklar, unnahbar, unruhig, unschlüssig, unsicher, unwohl, unzufrieden,  verängstigt, verärgert, verbittert, verkrampft, verlegen, verletzbar, verletzt, verloren, verrückt,  verschlafen, verschlossen, verschreckt, verspannt, verstimmt, verstört, verunsichert, verwirrt, verzagt,  verzweifelt,vorwurfsvoll,weinerlich,widerwillig,wütend,wutentbrannt, zappelig, zerknirscht, zermürbt, zerrissen, zittrig, zögerlich, zornig, zurückgewiesen, zynisch";
+  var txtFile = "aber, wenn, vielleicht, hätte, könnte, kann sein, mag sein, wollte, sollte, müsste, würde, aber, womöglich, möglicherweise, halt, man, im prinzip, prinzipiell, quasi, in der Regel, anscheinend, scheinbar, eigentlich, etwa, ungefähr, fast, gewiss, gelegentlich, grundsätzlich, glauben, meinen, finden, in etwa, in gewisser Weise, irgendwie, kaum, normalerweise, offenbar, relativ, selbstredend, sicherlich, bestimmt, wahrscheinlich, wenigstens, zumindest, sowieso, ziemlich, wirklich, wohl, überhaupt, übrigens, mal, einfach" ;
 
-  words = txtFile.split(",");
-  for (var i = 0; i < words.length; i++) {
-    words[i] = words[i].trim();
+  badWords = txtFile.split(",");
+  for (var i = 0; i < badWords.length; i++) {
+    badWords[i] = badWords[i].trim();
   }
 
 }
